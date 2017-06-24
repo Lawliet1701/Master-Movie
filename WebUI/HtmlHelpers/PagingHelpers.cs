6cc6cfbe18a5da8c5b9fb16982ecef4f5fa3918d
Lawliet1701 <mangekuu16@gmail.com>
2017-06-24 14:52:24 +0300

    Change page navigation

--- WebUI/HtmlHelpers/PagingHelpers.cs
+++ WebUI/HtmlHelpers/PagingHelpers.cs
@@ -17,16 +17,40 @@ namespace WebUI.HtmlHelpers
         {
             StringBuilder result = new StringBuilder();
 
-            for (int i = 1; i <= pagingInfo.TotalPages; i++)
+            //for (int i = 1; i <= pagingInfo.TotalPages; i++)
+            //{
+            //    TagBuilder tag = new TagBuilder("a");
+            //    tag.MergeAttribute("href", pageUrl(i));
+            //    tag.InnerHtml = i.ToString();
+            //    if (i == pagingInfo.CurrentPage)
+            //        tag.AddCssClass("selected");
+            //    result.Append(tag.ToString());
+            //}
+
+            TagBuilder tag = new TagBuilder("a");
+
+            if (pagingInfo.CurrentPage != 1)
             {
-                TagBuilder tag = new TagBuilder("a");
-                tag.MergeAttribute("href", pageUrl(i));
-                tag.InnerHtml = i.ToString();
-                if (i == pagingInfo.CurrentPage)
-                    tag.AddCssClass("selected");
+                tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(pagingInfo.CurrentPage - 1));
+                tag.InnerHtml = "Previous page";
                 result.Append(tag.ToString());
             }
 
+            tag = new TagBuilder("a");
+            tag.MergeAttribute("href", pageUrl(pagingInfo.CurrentPage));
+            tag.InnerHtml = pagingInfo.CurrentPage.ToString();
+            tag.AddCssClass("selected");
+            result.Append(tag.ToString());
+
+            if (pagingInfo.CurrentPage != pagingInfo.TotalPages)
+            {
+                tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(pagingInfo.CurrentPage + 1));
+                tag.InnerHtml = "Next page";
+                result.Append(tag.ToString());
+            }
+           
             return MvcHtmlString.Create(result.ToString());
         }
     }
